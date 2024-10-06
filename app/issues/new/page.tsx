13b702d8cce285c9c5c0157f5802d32781bd772b
Yassine Saindou <yassinesaindou12@gmail.com>
2024-10-06 18:07:57 +0530

    Add a merkdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,14 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+'use client'
+import { Button, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 export default function NewIssuePage() {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title" />
-          <TextArea placeholder="Decription" />
-          <Button>Submit New Issue</Button>
+      <SimpleMDE placeholder="Decription" />
+      <Button>Submit New Issue</Button>
     </div>
   );
 }
