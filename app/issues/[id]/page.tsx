389da1dc58147715eb7fe16bb8417ee6f344411a
Yassine Saindou <yassinesaindou12@gmail.com>
2024-10-08 22:19:29 +0530

    Add markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,7 +2,7 @@ import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-
+import ReactMarkdown from "react-markdown";
 interface Props {
   params: { id: string };
 }
@@ -22,7 +22,9 @@ export default async function IssueDetailPage({ params }: Props) {
 
         <Text>{issue.createtAt.toDateString()}</Text>
       </Flex>
-      <Card>{issue.description}</Card>
+          <Card className="prose " mt={'4'}>
+          <ReactMarkdown >{issue.description}</ReactMarkdown>    
+          </Card>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+   // eslint-disable-next-line @typescript-eslint/no-require-imports
+   require('@tailwindcss/typography') ,
+    // ...
+  ],
 };
 export default config;
